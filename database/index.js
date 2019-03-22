a926ea14b595f880b83f325ff5a283d4914743b4
Kevin Pozzi <kevindavidpozzi@gmail.com>
2019-03-22 16:09:34 -0700

    set up remote connection to database being hosted on AWS

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const { password } = require('../password.js');
 
 const client = new Client({
   user: 'kevinpozzi',
-  host: 'localhost',
+  host: '13.56.18.69',
   database: 'openforcedb',
   password: password,
   port: 5432,
